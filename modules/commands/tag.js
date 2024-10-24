c5f8c2b49599e2a284210c860c02f27facad63de
tanvir0FS <110109590+tanvir0FS@users.noreply.github.com>
2024-10-24 23:23:15 +0600

    Update tag.js

--- modules/commands/tag.js
+++ modules/commands/tag.js
@@ -1,44 +1,45 @@
 const config = {
-  name: "tag",
-  version: "1.6.9",
-  author: "Dipto",
-  credits: "Dipto",
-  countDown: 0,
-  role: 0,
-  permission: 0,
-  description: "Tag user",
-  category: "tag",
-  commandCategory: "tag",
-  usePrefix: false,
-  guide: "{pn} [reply/mention]",
-  usages: "reply or mention"
-};
+        name: "tag",
+        version: "1.6.9",
+        author: "Dipto",
+        credits: "Dipto",
+        countDown: 2,
+        cooldown : 2,
+        role: 0,
+    		usePrefix: false,
+        hasPermission: 0,
+        description: "Tag user",
+        category: "tag",
+        commandCategory: "tag",
+        guide: "{pn} [reply/mention]",
+        usages: "reply or mention"
+    };
 
-module.exports.run = async ({ api, args, event }) => {
- try {
- const ID = event.messageReply.senderID || args[0];
- const mentionedUser = await api.getUserInfo(ID);
- if (mentionedUser && mentionedUser[ID]) {
- const userName = mentionedUser[ID].name;
- const text = args.join(" ");
- await api.sendMessage({
-  body: `${userName} ${text}`,
-  mentions: [{
-      tag: userName,
-      id: ID 
-   }]
- }, event.threadID, event.messageID);
-} else {
- api.sendMessage("Ukii 🐱", event.threadID, event.messageID);
- }
-} catch (error) {
-  console.log(error);
-  api.sendMessage(`Error:  ${error.message}`, event.threadID, event.messageID);
-}
-};
+const run = async ({ api, args, event }) => {
+       try {
+       const ID = event.messageReply.senderID || args[0];
+       const mentionedUser = await api.getUserInfo(ID);
+       if (mentionedUser && mentionedUser[ID]) {
+       const userName = mentionedUser[ID].name;
+       const text = args.join(" ");
+       await api.sendMessage({
+        body: `${userName} ${text}`,
+        mentions: [{
+            tag: userName,
+            id: ID 
+         }]
+       }, event.threadID, event.messageID);
+     } else {
+       api.sendMessage("Reply to a message to tag...", event.threadID, event.messageID);
+       }
+    } catch (error) {
+        console.log(error);
+        api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
+   }
+  };
 
 module.exports = {
-config, 
-onStart,
-run: onStart
+  config, 
+  onStart,
+  run: onStart
 };
