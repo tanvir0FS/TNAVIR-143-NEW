1062c7099b6829df7f7ab2fbedefac51d77e0f15
tanvir0FS <110109590+tanvir0FS@users.noreply.github.com>
2024-10-25 04:14:49 +0600

    Update and rename poli.js to make.js

--- modules/commands/make.js
+++ modules/commands/make.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "gen",
+  name: "make",
   version: "1.0.",
   hasPermssion: 0,
   credits: "jameslim",
@@ -15,13 +15,14 @@ const axios = require('axios');
 const fs = require('fs-extra');
  let { threadID, messageID } = event;
   let query = args.join(" ");
-  if (!query) return api.sendMessage("put text/query", threadID, messageID);
+  if (!query) return api.sendMessage("[🤍] 𝘗𝘭𝘦𝘢𝘴𝘦 𝘱𝘳𝘰𝘷𝘪𝘥𝘦 𝘺𝘰𝘶𝘳 𝘱𝘳𝘰𝘮𝘰𝘵𝘦.", threadID, messageID);
+  api.sendMessage("[🤍] 𝘐𝘮𝘢𝘨𝘦 𝘨𝘦𝘯𝘦𝘳𝘢𝘵𝘪𝘯𝘨,  𝘸𝘢𝘪𝘵...",event.threadID, event.messageID);
 let path = __dirname + `/cache/poli.png`;
   const poli = (await axios.get(`https://image.pollinations.ai/prompt/${query}`, {
     responseType: "arraybuffer",
   })).data;
   fs.writeFileSync(path, Buffer.from(poli, "utf-8"));
   api.sendMessage({
-    body: `[🤍] 𝘗𝘰𝘭𝘪 𝘈𝘪 𝘎𝘦𝘯𝘦𝘳𝘢𝘵𝘦𝘥 𝘚𝘶𝘤𝘤𝘦𝘴𝘴𝘧𝘶𝘭`,
+    body: `[🤍] 𝘐𝘮𝘢𝘨𝘦 𝘎𝘦𝘯𝘦𝘳𝘢𝘵𝘦𝘥 𝘚𝘶𝘤𝘤𝘦𝘴𝘴𝘧𝘶𝘭`,
     attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID);
 };
