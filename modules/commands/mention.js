4380b2f5cd719216512ff055ca7478d3dba49da8
tanvir0FS <110109590+tanvir0FS@users.noreply.github.com>
2024-10-25 15:56:51 +0600

    Create mention.js

---
+++ modules/commands/mention.js
@@ -0,0 +1,51 @@
+module.exports.config = {
+	name: "test1",
+	version: "1.0.2",
+	hasPermssion: 2,
+	credits: "Tanvir",
+	usePrefix: false,
+	description: "",
+	commandCategory: "without prefix",
+	usages: "[tag]",
+	cooldowns: 5
+};
+
+module.exports.run = async function({ api, args, Users, event}) {
+    var tanvir143 = Object.keys(event.mentions)[0];
+    if(!tanvir143) return api.sendMessage("tag member", event.threadID);
+ let name =  event.mentions[tanvir143];
+    var arraytag = [];
+        arraytag.push({id: mention, tag: name});
+    var tanvir = function (tanvir) { api.sendMessage(tanvir, event.threadID); }
+a("Mention Started ✅");
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 3000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 4000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 5000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 6000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 7000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 8000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 9000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 10000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 11000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 12000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 13000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 14000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 15000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 16000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 17000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 18000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 19000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 20000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 21000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 22000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 23000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 24000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 25000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 26000);
+setTimeout(() => {tanvir({body: " " + " " + name, mentions: arraytag})}, 27000);
+setTimeout(() => {tanvir("✅ Mentioned Successful ")} , 29000);
+
+
+
+  
+  }
